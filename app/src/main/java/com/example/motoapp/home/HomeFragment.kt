548861ba36a95738bc848f5687cc93d4209357af
Tomek <tomo105@wp.pl>
2021-03-12 22:45:39 +0100

    add logout menu button to home activity

--- app/src/main/java/com/example/motoapp/home/HomeFragment.kt
+++ app/src/main/java/com/example/motoapp/home/HomeFragment.kt
@@ -2,15 +2,15 @@ package com.example.motoapp.home
 
 import android.os.Bundle
 import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.motoapp.R
 import com.example.motoapp.data.Car
 import com.example.motoapp.databinding.FragmentHomeBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class HomeFragment : Fragment(), CarAdapter.OnCarItemLongClick {
 
@@ -23,7 +23,14 @@ class HomeFragment : Fragment(), CarAdapter.OnCarItemLongClick {
     private val LOG_DEBUG = "LOG_DEBUG"
 
     private val homeVM by viewModels<HomeViewModel>()                                                               // using delegate
-    private val adapter = CarAdapter(this)
+    private val adapter = CarAdapter(this)                                                                   // listening to our fragment
+
+    private val fbAuth = FirebaseAuth.getInstance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setHasOptionsMenu(true)
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentHomeBinding.inflate(layoutInflater)
@@ -31,13 +38,31 @@ class HomeFragment : Fragment(), CarAdapter.OnCarItemLongClick {
         return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.logout_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.logout_action -> {
+                fbAuth.signOut()
+                requireActivity().finish()
+            }
+
+        }
+        return false
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.recyclerViewHome.layoutManager = LinearLayoutManager(requireContext())
         binding.recyclerViewHome.adapter = adapter
+
     }
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+                                                                                                                                // observe on liveData
         homeVM.cars.observe(viewLifecycleOwner, {list ->
             adapter.setCars(list)
         })
@@ -50,7 +75,7 @@ class HomeFragment : Fragment(), CarAdapter.OnCarItemLongClick {
     }
 
     override fun onCarLongClick(car: Car, position: Int) {
-        Toast.makeText(requireContext(),car.name, Toast.LENGTH_LONG)
+        Toast.makeText(requireContext(),car.name, Toast.LENGTH_LONG)                                                            // show what we clicked
             .show()
     }
 
