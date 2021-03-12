b7185c1560449f8f515dc357006435234564ffb5
Tomek <tomo105@wp.pl>
2021-03-12 22:06:13 +0100

    use view Binding in adapter

--- app/src/main/java/com/example/motoapp/home/CarAdapter.kt
+++ app/src/main/java/com/example/motoapp/home/CarAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.motoapp.home
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +9,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.motoapp.R
 import com.example.motoapp.data.Car
+import com.example.motoapp.databinding.ListRowBinding
 
 class CarAdapter(private val listener: OnCarItemLongClick) :
     RecyclerView.Adapter<CarAdapter.CarViewHolder>() {
@@ -21,23 +23,36 @@ class CarAdapter(private val listener: OnCarItemLongClick) :
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CarViewHolder {
-        val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.list_row, parent, false)
-        return CarViewHolder(view)
+        val binding = ListRowBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return CarViewHolder(binding)
+
+        //  val inflater = LayoutInflater.from(parent.context)
+        // val view = inflater.inflate(R.layout.list_row, parent, false)
+        //  return CarViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: CarViewHolder, position: Int) {
-        bindData(holder)
+        bindData(holder, position)
+
     }
 
-    private fun bindData(holder: CarViewHolder) {
-        val name = holder.itemView.findViewById<TextView>(R.id.carName)
-        val productionYear = holder.itemView.findViewById<TextView>(R.id.carProductionYear)
-        val image = holder.itemView.findViewById<ImageView>(R.id.carImage)
+    private fun bindData(holder: CarViewHolder, position: Int) {
+        //  val name = holder.itemView.findViewById<TextView>(R.id.carName)
+        //  val productionYear = holder.itemView.findViewById<TextView>(R.id.carProductionYear)
+        //  val image = holder.itemView.findViewById<ImageView>(R.id.carImage)
 
-        name.text = carsList[holder.adapterPosition].name
-        productionYear.text = carsList[holder.adapterPosition].productionYear
-        // car image TODO
+        // name.text = carsList[holder.adapterPosition].name
+        // productionYear.text = carsList[holder.adapterPosition].productionYear
+
+        with(holder) {
+            with(carsList[position]) {
+                binding.carName.text = name
+                binding.carProductionYear.text = productionYear
+                // binding.carImage
+                // car image TODO
+            }
+        }
+
 
     }
 
@@ -45,9 +60,11 @@ class CarAdapter(private val listener: OnCarItemLongClick) :
         return carsList.size
     }
 
-    inner class CarViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    inner class CarViewHolder(val binding: ListRowBinding) : RecyclerView.ViewHolder(binding.root) {
+
         init {
-            view.setOnLongClickListener() {
+            binding.root.setOnLongClickListener() {
+                Log.d("LOG_DEBUG", "elko w listenerze")
                 listener.onCarLongClick(carsList[adapterPosition], adapterPosition)
                 true
             }
